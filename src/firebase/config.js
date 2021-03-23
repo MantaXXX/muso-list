daa4d768c89859f426e774a625cd27bed0ee8b8d
Jia <pochialu@gmail.com>
2021-03-23 14:12:52 +0800

    feat: set up the firebase project

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhzj9RMqNccNheHyfUjfX3S3q_9VQFzIk",
+  authDomain: "muso-list.firebaseapp.com",
+  projectId: "muso-list",
+  storageBucket: "muso-list.appspot.com",
+  messagingSenderId: "993911519324",
+  appId: "1:993911519324:web:b36356d77cd09cea074e99"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+
+export { projectFirestore, projectAuth, timestamp }
