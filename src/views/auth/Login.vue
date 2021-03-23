a5efa31799e6938c78e4f438eb9a05f742492bee
Jia <pochialu@gmail.com>
2021-03-23 15:32:59 +0800

    feat: create login component

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h3>Login</h3>
+    <input type="email" placeholder="email" v-model="email" />
+    <input type="password" placeholder="password" v-model="passowrd" />
+    <div class="error" v-if="error">{{ error }}</div>
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import useLogin from "@/composables/useLogin";
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const { error, login } = useLogin();
+
+    const email = ref("");
+    const password = ref("");
+
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value);
+      if (!error.value) {
+        console.log("user logged in");
+      }
+    };
+    return { email, password, handleSubmit, error };
+  },
+};
+</script>
+
+<style>
+</style>
